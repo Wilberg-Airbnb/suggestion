07e076b444ab74784bfcf13f31a1be8919de9051
EC2 Default User <ec2-user@ip-172-31-41-75.us-east-2.compute.internal>
2020-07-29 00:45:55 +0000

    fix docker npm isseu

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const compression = require('compression');
 const app = express();
 app.use(compression());
 app.use(cors());
-// app.use(express.static('public/dist'));
+app.use(express.static('public/dist'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
